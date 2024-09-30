49b3b8bd3717ee482aa3fbeb64459220e1ca035e
Jonah Ludke <jonahludke@gmail.com>
2024-09-29 22:05:21 -0400

    feat: a human was added

--- src/components/House.js
+++ src/components/House.js
@@ -1,47 +1,61 @@
 // src/components/House.js
 import React, { useState } from 'react';
+import PixelHuman from './PixelHuman';
 
 const House = () => {
-  const [position, setPosition] = useState({ x: 700, y: 50 }); // Initial position of the house
+  const [position, setPosition] = useState({ x: 700, y: 50 });
+  const [isPlaced, setIsPlaced] = useState(false); // Detect when house is placed
+  const [showHuman, setShowHuman] = useState(false);
 
   const handleDragStart = (e) => {
     const img = new Image();
     e.dataTransfer.setDragImage(img, 0, 0); // Hide default drag image
+    setShowHuman(false); // Hide human while dragging
   };
 
-  const handleDrag = (e) => {
-    if (e.clientX === 0 && e.clientY === 0) return; // Prevent updating position if drag ends
-    setPosition({ x: e.clientX - 16, y: e.clientY - 16 }); // Center the drag to the mouse
+  const handleDrop = (e) => {
+    setPosition({ x: e.clientX - 16, y: e.clientY - 16 });
+    setIsPlaced(true);
+    setShowHuman(true); // Show human when house is placed
   };
 
   return (
-    <div
-      draggable
-      onDragStart={handleDragStart}
-      onDrag={handleDrag}
-      style={{
-        width: '32px',
-        height: '32px',
-        backgroundColor: '#8B4513', // Brown color for the house base
-        position: 'absolute',
-        top: `${position.y}px`,
-        left: `${position.x}px`,
-        cursor: 'grab',
-      }}
-    >
+    <>
       <div
-        style={{
-          width: '0',
-          height: '0',
-          borderLeft: '16px solid transparent',
-          borderRight: '16px solid transparent',
-          borderBottom: '16px solid #FFD700', // Yellow roof color
-          position: 'relative',
-          top: '-16px',
-          left: '0px',
+        draggable
+        onDragStart={handleDragStart}
+        onDrag={(e) => {
+          if (e.clientX === 0 && e.clientY === 0) return; // Ignore empty drag
+          setPosition({ x: e.clientX - 16, y: e.clientY - 16 });
         }}
-      />
-    </div>
+        onDragEnd={handleDrop}
+        style={{
+          width: '32px',
+          height: '32px',
+          backgroundColor: '#8B4513', // Brown for house
+          position: 'absolute',
+          top: `${position.y}px`,
+          left: `${position.x}px`,
+          cursor: 'grab',
+        }}
+      >
+        <div
+          style={{
+            width: '0',
+            height: '0',
+            borderLeft: '16px solid transparent',
+            borderRight: '16px solid transparent',
+            borderBottom: '16px solid #FFD700', // Yellow roof
+            position: 'relative',
+            top: '-16px',
+            left: '0px',
+          }}
+        />
+      </div>
+
+      {/* Render the human only if the house is placed */}
+      {showHuman && <PixelHuman housePosition={position} />}
+    </>
   );
 };
 
